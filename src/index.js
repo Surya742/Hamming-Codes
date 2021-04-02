d172e1ed81c14c875700c9df602925d5dcd52721
Surya742 <suryakumarsahani.742@gmail.com>
2021-04-02 17:07:41 +0530

    Project Added to Github

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import HammingCodes from './components/HammingCodes';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <HammingCodes />
   </React.StrictMode>,
   document.getElementById('root')
 );
